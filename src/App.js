f6e6b997017a42027c229b2e508b421bd78939ac
sawadanoritaka <snp.inc.info@gmail.com>
2021-04-24 13:37:38 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import logo from './S__2277548.jpg';
+import { withAuthenticator, AmplifySignOut,AmplifySignIn } from '@aws-amplify/ui-react'
 const App = () => {
   return (
     <div>
+      <AmplifySignOut />
       <h1>
-        ホイップ
+        シロ
       </h1>
+      <img src={logo} alt="Logo" />
+  
     </div>
   )
 }
 
-export default App
+export default withAuthenticator(App);
